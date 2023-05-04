099c27161fd63d285adedc2274203479c4543e27
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-05-04 21:10:08 +0300

    Add project files.

---
+++ MerkezBank.Data/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using MerkezBank.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerkezBank.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Entities> Entities { get; set; }
+    }
+}
+
---
+++ MerkezBank.Data/Entity/Entities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerkezBank.Data.Entity
+{
+    public class Entities
+    {
+        [Key]
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        public string USD { get; set; }
+
+        public string AUD { get; set; }
+
+        public string EUR { get; set; }
+
+        public string GBP { get; set; }
+
+        public string CurrencyDate { get; set; }
+
+        private DateTime _createDate = DateTime.Now;
+        public DateTime CreateDate { get => _createDate; set => value = _createDate; }
+    }
+}
